f91cf17d25738ea25ef6dbaa5860ffab4e46b81d
Diana Zinevych <dianazinevych02@gmail.com>
2025-08-06 11:26:41 +0200

    feat: add inputs and texterea in Form component, add necessary liberies

--- src/components/BookForm/BookForm.jsx
+++ src/components/BookForm/BookForm.jsx
@@ -1,11 +1,51 @@
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import DatePicker from "react-datepicker";
+import toast from 'react-hot-toast';
 import * as Yup from "yup";
+import Button from '../Button/Button.jsx';
+import css from "./BookForm.module.css";
+
+const FeedbackSchema = Yup.object().shape({
+    name: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
+    email: Yup.string().email("Must be a valid email!").required("Required"),
+    date: Yup.date().nullable().min(new Date(), "Date cannot be in the past"),
+    comment: Yup.string().max(256, "Too long")
+});
+const initialValues = {
+    name: "",
+    email: '',
+    date: null,
+    comment: ""
+}
 
 export default function BookForm() {
+    const handleSubmit = (values, actions) => {
+    actions.resetForm();
+    toast.success("")
+}
     return (
-        <div>
-            <Formik></Formik>
-            
+        <div className={css.bookForm}>
+            <Formik initialValues={initialValues} onSubmit={handleSubmit} validationSchema={FeedbackSchema}>
+                <Form className={css.form}>
+                    <div className={css.inputWrapper}>
+                    <Field className={css.input} type="text" name="name" placeholder="Name" />
+                        <ErrorMessage name="name" component="span" className="error" />
+                    </div>
+                    <div className={css.inputWrapper}>
+                    <Field className={css.input} type="email" name="email" placeholder="Email" />
+                    <ErrorMessage name="email" component="span" className="error" />
+                    </div>
+                    <div className={css.inputWrapper}>
+                <Field className={css.input} type="date" name="date" placeholder="Booking date" />
+                <ErrorMessage name="date" component="span" className="error" />
+                    </div>
+                    <div className={css.inputWrapper}>
+                    <Field className={css.textarea} as="textarea" name="comment" placeholder="Comment" />
+        <ErrorMessage name="comment" component="span" className="error" />
+                    </div>
+                    <Button type='submit'>Send</Button>
+                </Form>
+            </Formik>
 </div>
     );
 }
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -4,9 +4,9 @@ import LinkButton from "../../components/LinkButton/LinkButton.jsx";
 export default function MainPage() {
     return (
         <section className={css.section}>
-            <Container>
+            <Container >
             <h1 className={css.title}>Find your perfect rental car</h1>
-            <p className={css.description}>Reliable and budget-friendly rentals for any journey</p>
+                    <p className={css.description}>Reliable and budget-friendly rentals for any journey</p>
             <div className={css.linkbtn}>
                     <LinkButton to={`/catalog`}>View Catalog</LinkButton>
                 </div>
